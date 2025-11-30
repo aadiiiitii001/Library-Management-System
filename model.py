66cda505547db3890a2958ab022faff744ecb1f8
aadiiiitii001 <153495639+aadiiiitii001@users.noreply.github.com>
2025-11-30 12:29:47 +0530

    Update model.py

--- model.py
+++ model.py
@@ -7,12 +7,18 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     author = db.Column(db.String(100))
-    available = db.Column(db.Boolean, default=True)
+    isbn = db.Column(db.String(20), unique=True)
+    category = db.Column(db.String(50))
+    quantity = db.Column(db.Integer, default=1)
+    available = db.Column(db.Integer, default=1)  # number of copies available
+    cover_img = db.Column(db.String(200))
 
 class Member(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
+    phone = db.Column(db.String(20))
+    membership_type = db.Column(db.String(20), default="Student")
 
 class Issue(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -20,6 +26,8 @@ class Issue(db.Model):
     member_id = db.Column(db.Integer, db.ForeignKey('member.id'))
     issue_date = db.Column(db.DateTime, default=datetime.utcnow)
     return_date = db.Column(db.DateTime, nullable=True)
+    due_date = db.Column(db.DateTime)
+    fine = db.Column(db.Integer, default=0)
 
     book = db.relationship('Book', backref='issues')
     member = db.relationship('Member', backref='issues')
